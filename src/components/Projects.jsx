dc24bb507de2ef9ff93634f4d9741bc5943f00ac
mickijames <micalynjeanette@eagles.ewu.edu>
2018-12-05 01:16:01 -0800

    Added videos and links to projects
    + Added links for sites/prototype and videos for games
    + Better styling for project text
    + Better organization for projects/cascading children

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,18 @@
 ﻿import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import ReactPlayer from 'react-player';
 import { Segment, Image } from 'semantic-ui-react';
 import ReactModal from 'react-modal';
 import tetris from '../Assets/tetris.png';
+import tetrisVideo from '../Assets/tetris.mp4';
 import ashman from '../Assets/ashman_play_cropped.png';
+import ashmanVideo from '../Assets/ashman.mp4';
 import shoppingCart from '../Assets/shop_home.png';
 import plannerApp from '../Assets/home.png';
 
 ReactModal.setAppElement('#root');
 
 export default class Projects extends Component {
-    constructor () {
-        super();
-        this.state = {
-            showModal: false
-        };
-    }
-    
-    handleOpenModal = (e) => {
-        this.setState({ showModal: true });
-        console.log(e);
-    }
-    
-    handleCloseModal = () => {
-        this.setState({ showModal: false });
-    }
-
-    getParent() {
-        return document.querySelector('#projects');
-    }
-
     pos;
     componentDidMount() {
         let n = ReactDOM.findDOMNode(this);
@@ -47,25 +30,40 @@ export default class Projects extends Component {
                     <Segment padded="very">
                         <h2>Projects</h2>
                         <Segment.Group horizontal padded="very">
-                            <Project onClick={this.handleOpenModal} path={ashman} size="medium" name="Ashman Game" desc="I created this pacman-like game for my final project in my android class." />
-                            <Project onClick={this.handleOpenModal} path={tetris} size="large" name="Tetris Game" desc="This is a tetris game created with the C#, WPF framework. Ethan Holman and I worked on it together as a final project." />
-                            </Segment.Group>
+                            <Project 
+                                name="Good Morning Planner" 
+                                path={plannerApp}
+                                size="medium" 
+                                desc="I designed this app as a one-stop shop for planning your day. It combines the weather, to-do/reminder apps, calendar, and alarm into a single app for better organization." 
+                                type="View my prototype" 
+                                link="https://invis.io/7APCOPCP9Q8" 
+                            />
+                            <Project 
+                                name="Ashman Game" 
+                                path={ashman} 
+                                size="medium" 
+                                desc="I created this pacman-like game for my final project in my android class." 
+                            />
+                        </Segment.Group>
                         <Segment.Group horizontal padded="very">
-                            <Project onClick={this.handleOpenModal} path={plannerApp} size="medium" name="Good Morning Planner" desc="I designed this app as a one-stop shop for planning your day. It combines the weather, to-do/reminder apps, calendar, and alarm into a single app for better organization." />
-                            <Project onClick={this.handleOpenModal} path={shoppingCart} size="large" name="Shopping Cart Website" desc="I created a data-driven website with C# ASP.NET and Bootstrap." />
+                            <Project 
+                                name="Tetris Game" 
+                                path={tetris} 
+                                size="large" 
+                                desc="This is a tetris game created with the C#, WPF framework. Ethan Holman and I worked on it together as a final project." 
+                            />
+                            <Project 
+                                name="Shopping Cart Website" 
+                                path={shoppingCart} 
+                                size="large" 
+                                desc="I created a data-driven website with C# ASP.NET and Bootstrap." 
+                                type="View my website" 
+                                link="http://micalynjeanette.com/shoppingcart/" 
+                                
+                            />
                         </Segment.Group>
                     </Segment>
                 </Segment.Group>
-                <ReactModal 
-                    isOpen={this.state.showModal}
-                    contentLabel="project modal"
-                    onRequestClose={this.handleCloseModal}
-                    shouldCloseOnOverlayClick={true}
-                    shouldCloseOnEsc={true}
-                    parentSelector={this.getParent}
-                >
-                    <ProjectModal />
-                </ReactModal>
             </div>
         );
     }
@@ -74,17 +72,45 @@ export default class Projects extends Component {
 function Project(props) {
     return (
         <Segment padded="very" onClick={props.onClick} >
-            <Image src={props.path} size={props.size} ui rounded />
+            <Media name={props.name} link={props.link} path={props.path} size={props.size} />
             <div className="text">
+                <a target="_blank" rel="noopener noreferrer" href={props.link}>{props.type}</a>
                 <h4>{props.name}</h4>
                 <p>{props.desc}</p>
-            </div>
+                </div>
         </Segment>
     );
 }
 
-function ProjectModal(props) {
+function Media(props) {
+    const name = props.name;
+    if (name === "Ashman Game") return <Player video={ashmanVideo} />;
+    else if (name === "Tetris Game") return <Player video={tetrisVideo} />;
+    else return <Img link={props.link} path={props.path} size={props.size} />;
+}
+
+class Img extends Component {
+    imageClick = () => {
+        window.open(this.props.link, "_blank")
+    }
+
+    render() {
+        return(
+            <Image onClick={this.imageClick} src={this.props.path} size={this.props.size} ui rounded />
+        );
+    }
+}
+
+function Player(props) {
+    const vid = props.video;
     return(
-        <h3>Modal {props.name}</h3>
+        <ReactPlayer controls muted
+            className='react-player'
+            url={[
+                { src: vid, type: "video/mp4" }
+            ]}
+            width='100%'
+            height=''
+        />
     );
 }
