c0f2a3a7c3377eb2bca92be6cf0314a8b576e6fd
mickijames <micalynjeanette@eagles.ewu.edu>
2019-03-25 21:14:29 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mywebsite/precache-manifest.ccb8653d6073db13ccf69f256f60f56f.js"
+  "/mywebsite/precache-manifest.694dcfcb9ec1b2e6d1119970cb330135.js"
 );
 
 workbox.clientsClaim();
